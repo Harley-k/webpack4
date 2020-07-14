af9e457ac738c827444765cd9de70d54da1fac0b
Harley-K <harley-k@Harley-KdeMacBook-Pro.local>
2020-07-15 06:45:50 +0800

    多入口多出口配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,16 @@ module.exports={
     // entry:'./src/index.js', //单入单出
     // entry:['./src/index.js','./src/main.js'],//多入单出
      entry:{
-        
-     },
+        page1:'./src/page1/index.js',
+        page2:'./src/page2/index.js'
+     }, //这里注意一下,多入口和多出口必须要配合
 
     // 输出文件
     output:{
         // 输出路径
         path:path.resolve(__dirname,'dist'),
         // 输出文件名
-        filename:'index.js'
+        //filename:'index.js' //单出口配置
+        filename:'[name].js'//多出口配置
     }
 }
