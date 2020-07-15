5eddf6f01b48d82b31eca236cb80f72c5005f8ca
Harley-K <harley-k@Harley-KdeMacBook-Pro.local>
2020-07-16 06:49:08 +0800

    插件使用

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 loader让 webpack 能够去处理那些非 JavaScript 文件（webpack 自身只理解 JavaScript）。loader 可以将所有类型的文件转换为 webpack 能够处理的有效模块，然后你就可以利用 webpack 的打包能力，对它们进行处理。
+
+
+html-webpack-plugin 配置
+
+<a href="https://github.com/DanielRuf/html-minifier-terser">minify配置</a>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const miniCss = require('mini-css-extract-plugin')
 module.exports = {
     // 入口文件
     entry: './src/index.js', //单入单出
@@ -13,7 +15,7 @@ module.exports = {
         // 输出路径
         path: path.resolve(__dirname, 'dist'),
         // 输出文件名
-        filename: 'index.js' //单出口配置
+        filename: 'index-[hash].js' //单出口配置
         // filename:'[name].js'//多出口配置
     },
     devServer: {
@@ -28,6 +30,7 @@ module.exports = {
                 test: /\.css$/, //这是一个正则,匹配到要处理的文件
                 use: [ //匹配到的文件使用哪个loader,这里使用了两个,注意要把style-loader丢在前面
                     'style-loader',
+                    miniCss.loader,
                     'css-loader',
                     {
                         loader: 'postcss-loader',//这个loader需要搭配autoperfixer一起使用
@@ -37,36 +40,55 @@ module.exports = {
                             ],
 
                         }
-                    }
+                    },
+                    
                 ]
             },
             // 处理图片
             {
-                test:/\.(png|jpg|jpeg|gif)$/,//匹配需要处理的文件
-                use:{
-                    loader:'file-loader',
-                    options:{
-                        name:'[name]-[hash].[ext]',//处理文件名 name就是文件名,hase是hase,ext是后缀
+                test: /\.(png|jpg|jpeg|gif)$/,//匹配需要处理的文件
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name]-[hash].[ext]',//处理文件名 name就是文件名,hase是hase,ext是后缀
                         //context:'',//webpack的上下文
                         //publicPath:'',//文件public发布目录=>也是就是说这个属性设置的是发布后的文件路径比如:www.baidu.com/img/
-                        outputPath:'image',//自定义output输出目录=>打包后的文件目录
+                        outputPath: 'image',//自定义output输出目录=>打包后的文件目录
                     }
                 } // 使用file-loader
             },
             // 处理字体文件
             {
-                test:/\.(ttf|woff|woff2|eot|svg)$/,//匹配需要处理的文件
-                use:{
-                    loader:'file-loader',
-                    options:{
-                        name:'[name]-[hash].[ext]',//处理文件名 name就是文件名,hase是hase,ext是后缀
+                test: /\.(ttf|woff|woff2|eot|svg)$/,//匹配需要处理的文件
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name]-[hash].[ext]',//处理文件名 name就是文件名,hase是hase,ext是后缀
                         //context:'',//webpack的上下文
                         //publicPath:'',//文件public发布目录=>也是就是说这个属性设置的是发布后的文件路径比如:www.baidu.com/img/
-                        outputPath:'font',//自定义output输出目录=>打包后的文件目录
+                        outputPath: 'font',//自定义output输出目录=>打包后的文件目录
                     }
                 } // 使用file-loader
             }
         ]
     },
-    
+    plugins: [
+        new htmlWebpackPlugin({
+            title: 'htmlWebpackPlugin',//title
+            template: './src/index.html',//模板目录
+            filename: 'index.html',//打包后的文件名
+            minify: {
+                collapseWhitespace: true,//去除空格
+                removeComments: true,//去除注释
+                removeRedundantAttributes: true, //去除冗余属性
+                removeScriptTypeAttributes: false,//去除js类型属性
+                removeStyleLinkTypeAttributes: true,//去除css类型属性
+            },
+            has:true //引入生成资源的时候加入hash避免缓存
+        }),
+        new miniCss({
+            filename:'./css/[name]-[hash].css'
+        })
+    ]
+
 }
